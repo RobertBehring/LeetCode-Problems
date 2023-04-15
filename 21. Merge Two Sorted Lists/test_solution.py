1bdae6c8add0c7c6bfd3308a5cd1d71997198c62
RobertBehring <rsbehring@gmail.com>
2023-04-15 11:00:02 -0700

    add: 21. Merge Two Sorted Lists

---
+++ 21. Merge Two Sorted Lists/test_solution.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+import solution
+import listNode
+
+testFunc = solution.Solution().mergeTwoLists
+
+
+class TestSolution(TestCase):
+    def test(self):
+        testCases = [
+            [[1, 2, 4], [1, 3, 4], [1, 1, 2, 3, 4, 4]]
+        ]
+        for test in testCases:
+            for i
+            if testFunc(INSERT_ARGS_HERE) != test[INSERT_ANS_IDX_HERE]:
+                self.fail()
