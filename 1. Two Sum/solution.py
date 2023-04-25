926266e3f126b4f000b9c7cb37eb98c91ceca10a
RobertBehring <rsbehring@gmail.com>
2023-04-25 08:10:41 -0700

    update: added hashmap solution

--- 1. Two Sum/solution.py
+++ 1. Two Sum/solution.py
@@ -1,6 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # return self.bruteTwoSum(nums, target)
+        return self.mapTwoSum(nums, target)
+
+    def mapTwoSum(self, nums: list[int], target: int) -> list[int]:
+        """Time: O(n) | Space: O(n)"""
+        seen = dict()  # hashmap
+        n = len(nums)
+
+        for i in range(n):
+            diff = target - nums[i]
+            if diff in seen:
+                return [i, seen[diff]]
+            seen[nums[i]] = i
+
+    def bruteTwoSum(self, nums: list[int], target: int) -> list[int]:
+        """Time: O(n^2) | Space: O(1)"""
+        list_length = len(nums)
+
+        # Iterate through list one element at a time
+        for number in range(list_length - 1):
+            # Iterate through list [i+1:n]
+            for next_number in range(number + 1, list_length):
+                # Check if (i + j) == target
+                if nums[number] + nums[next_number] == target:
+                    return [number, next_number]
