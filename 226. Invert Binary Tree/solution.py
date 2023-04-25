6dda2a8d80c2736682efedf11cf0776d740687aa
RobertBehring <rsbehring@gmail.com>
2023-04-25 09:09:36 -0700

    update: added iter solution using queue

--- 226. Invert Binary Tree/solution.py
+++ 226. Invert Binary Tree/solution.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -16,5 +19,23 @@ class Solution:
 
             node.left, node.right = node.right, node.left
 
-        potInvert(root)
+        # potInvert(root)
+        self.iterInvertTree(root)
+        return root
+
+    def iterInvertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return
+        q = deque()
+        q.append(root)
+
+        while q:
+            cur = q.popleft()
+            cur.left, cur.right = cur.right, cur.left
+
+            if cur.left:
+                q.append(cur.left)
+            if cur.right:
+                q.append(cur.right)
+
         return root
