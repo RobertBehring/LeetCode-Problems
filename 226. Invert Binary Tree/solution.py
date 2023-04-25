67b94e2e085f9bf04b195d640a8b28e369ea842e
RobertBehring <rsbehring@gmail.com>
2023-04-25 08:54:27 -0700

    226. Invert Binary Tree

---
+++ 226. Invert Binary Tree/solution.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def potInvert(node):
+            """DFS POT of binary tree"""
+            if not node:
+                return
+
+            potInvert(node.left)
+            potInvert(node.right)
+
+            node.left, node.right = node.right, node.left
+
+        potInvert(root)
+        return root
