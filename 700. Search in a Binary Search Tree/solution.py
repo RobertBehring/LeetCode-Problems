e3e3e03fa4f8b051f3b4ba7b16b8484b29a918c4
RobertBehring <rsbehring@gmail.com>
2023-04-26 13:57:19 -0700

    update: added editorial solutions for recursion and iteration

--- 700. Search in a Binary Search Tree/solution.py
+++ 700. Search in a Binary Search Tree/solution.py
@@ -5,12 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        if not root:
-            return
-        if root.val == val:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[
+        TreeNode]:
+        """Optimized LeetCode Editorial Solution recursion"""
+        if root is None or val == root.val:
             return root
-        if val < root.val:
-            return self.searchBST(root.left, val)
-        else:
-            return self.searchBST(root.right, val)
+
+        return self.searchBST(root.left,
+                              val) if val < root.val else self.searchBST(
+            root.right, val)
+
+        # """Optimized LeetCode Editorial Solution iteration"""
+        # while root is not None and root.val != val:
+        #     root = root.left if val < root.val else root.right
+        # return root
+
+        # """Homebrew Solution"""
+        # if not root:
+        #     return
+        # if root.val == val:
+        #     return root
+        # if val < root.val:
+        #     return self.searchBST(root.left, val)
+        # else:
+        #     return self.searchBST(root.right, val)
